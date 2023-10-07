2e3c5282d4c5df1adaa950f30efd495b3f86f4d5
WayneChen158 <ychen168@seas.upenn.edu>
2023-10-06 17:49:35 -0700

    1. Update pom file for correct dependencies 2. Update RawMaterialsRepository

--- src/main/java/com/atilaBiosystems/InventoryManagementSystem/repository/RawMaterialsRepository.java
+++ src/main/java/com/atilaBiosystems/InventoryManagementSystem/repository/RawMaterialsRepository.java
@@ -2,9 +2,10 @@ package com.atilaBiosystems.InventoryManagementSystem.repository;
 
 import com.atilaBiosystems.InventoryManagementSystem.entity.RawMaterial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
-
+@RepositoryRestResource(path="raw-materials")
 public interface RawMaterialsRepository extends JpaRepository<RawMaterial, Integer> {
 
 }
