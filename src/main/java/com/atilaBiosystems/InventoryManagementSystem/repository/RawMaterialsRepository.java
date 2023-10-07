fdaa57f34bc91f0602d5bbfdb589600441b495f0
WayneChen158 <ychen168@seas.upenn.edu>
2023-10-06 17:17:11 -0700

    1. bug fix

--- src/main/java/com/atilaBiosystems/InventoryManagementSystem/repository/RawMaterialsRepository.java
+++ src/main/java/com/atilaBiosystems/InventoryManagementSystem/repository/RawMaterialsRepository.java
@@ -1,10 +1,10 @@
 package com.atilaBiosystems.InventoryManagementSystem.repository;
 
-import com.atilaBiosystems.InventoryManagementSystem.entity.RawMaterials;
+import com.atilaBiosystems.InventoryManagementSystem.entity.RawMaterial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RawMaterialsRepository extends JpaRepository<RawMaterials, Integer> {
+public interface RawMaterialsRepository extends JpaRepository<RawMaterial, Integer> {
 
 }
