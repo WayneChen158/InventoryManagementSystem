417b8d885abc1ec9045c54a12e44e86f3abcc299
WayneChen158 <ychen168@seas.upenn.edu>
2023-10-28 20:17:31 -0700

    1. for revert

---
+++ src/main/java/com/atilaBiosystems/InventoryManagementSystem/Exception/InvalidManufactureRecordException.java
@@ -0,0 +1,7 @@
+package com.atilaBiosystems.InventoryManagementSystem.Exception;
+
+public class InvalidManufactureRecordException extends RuntimeException{
+    public InvalidManufactureRecordException(String message) {
+        super(message);
+    }
+}
