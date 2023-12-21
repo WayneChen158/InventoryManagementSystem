b49d8b19bb46cf2ea7d9ff43a619d478a7484530
Xinhao <songxinhao@hotmail.com>
2023-12-21 00:05:31 -0800

    Implement request inbound unit conversion logic

--- src/main/java/com/atilaBiosystems/InventoryManagementSystem/DAO/MarkRequestReceivedForm.java
+++ src/main/java/com/atilaBiosystems/InventoryManagementSystem/DAO/MarkRequestReceivedForm.java
@@ -4,43 +4,33 @@ public class MarkRequestReceivedForm {
     private int requestId;
     private double fulfilledAmount;
     private double receivedAmount;
+    private String unit;
+    private double inboundFactor;
     private String receivedBy;
     private String receivedDate;
     private int isFullyReceived;
     public int getRequestId() {
         return requestId;
     }
-    public void setRequestId(int requestId) {
-        this.requestId = requestId;
-    }
     public double getFulfilledAmount() {
         return fulfilledAmount;
-    }
-    public void setFulfilledAmount(double fulfilledAmount) {
-        this.fulfilledAmount = fulfilledAmount;
     }
     public double getReceivedAmount() {
         return receivedAmount;
     }
-    public void setReceivedAmount(double receivedAmount) {
-        this.receivedAmount = receivedAmount;
+    public String getUnit() {
+        return unit;
+    }
+    public double getInboundFactor() {
+        return inboundFactor;
     }
     public String getReceivedBy() {
         return receivedBy;
     }
-    public void setReceivedBy(String receivedBy) {
-        this.receivedBy = receivedBy;
-    }
     public String getReceivedDate() {
         return receivedDate;
     }
-    public void setReceivedDate(String receivedDate) {
-        this.receivedDate = receivedDate;
-    }
     public int getIsFullyReceived() {
         return isFullyReceived;
-    }
-    public void setIsFullyReceived(int isFullyReceived) {
-        this.isFullyReceived = isFullyReceived;
     }
 }
